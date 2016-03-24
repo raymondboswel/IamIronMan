6046bb218d7293540a7c9855f25fabc1e2d1a2d7
raymondboswel <raymondboswel@gmail.com>
2016-03-24 16:44:43 +0200

    Some tinkering..

--- iRironMan/running-log.R
+++ iRironMan/running-log.R
@@ -1,8 +1,22 @@
+library(lubridate)
+
 getData <- function () {
   download.file(url="https://docs.google.com/spreadsheets/d/1qyhjFF4CcjeSu__88_WOLjFumuguDmVWTHbOlrikEvI/pub?gid=0&single=true&output=csv",
-                   destfile="/home/dev/R/running-log.csv")
-  
-  
+                   destfile="running-log.csv")
 }
 
-rawdf <- read.csv("/home/dev/R/running-log.csv")
+createDataFrames <- function() {
+  
+  rawdf <- read.csv("running-log.csv")
+  runningdf <- rawdf[rawdf$Type == "Running" & rawdf$Pace != "00:00", ]
+  cyclingdf <- rawdf[rawdf$Type == "Cycling", ]
+  
+  runningPace <- runningdf[, c("Pace")]
+  View(runningPace)
+  View(period_to_seconds(ms(runningPace)))
+  runningPaceTs <- ts(period_to_seconds(ms(runningPace)))
+  plot.ts(runningPaceTs)
+}
+
+getData()
+createDataFrames()
